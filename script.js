e7b3f168bcbb39051bf31b6becbba7763fcedcc4
Phillipe Sobrinho <phillipe_sobrinho@hotmail.com>
2024-12-12 11:50:23 +0000

    Atualizando campo

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();  // Impede o envio tradicional do formulário e evita que a página recarregue
 
-    // Coletar dados do formulário
+
+
+// Coletar dados do formulário
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const description = document.getElementById('description').value;
 
-    // Validar campos
-    if (!name || !email || !description) {
-        document.getElementById('formMessage').innerHTML = 'Por favor, preencha todos os campos!';
-        document.getElementById('formMessage').style.color = 'red';  // Exibe a mensagem de erro em vermelho
-        return;
-    }
+ 
+ 
+// // Validar campos,  Falsy
+//     if (!name || !email || !description) {
+//         document.getElementById('formMessage').innerHTML = 'Por favor, preencha todos os campos!';
+//         return;
+//     }
 
-    // Se todos os campos estiverem preenchidos, exibe uma mensagem de confirmação simples
+    // Se todos os campos estiverem preenchidos, exibe uma mensagem 
     document.getElementById('formMessage').innerHTML = 'Formulário enviado com sucesso!';
     document.getElementById('formMessage').style.color = 'green';  // Exibe a mensagem de sucesso em verde
 });
